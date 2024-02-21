775c54b4e3980924b12e635dd6f015055f16c458
Ujwala-lute <117163783+Ujwala-lute@users.noreply.github.com>
2024-02-21 23:31:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-function show(type)
-{
-    count = 0;
-    for (var key in methods) {
-        var row = document.getElementById(key);
-        if ((methods[key] &  type) != 0) {
-            row.style.display = '';
-            row.className = (count++ % 2) ? rowColor : altColor;
-        }
-        else
-            row.style.display = 'none';
-    }
-    updateTabs(type);
-}
-
-function updateTabs(type)
-{
-    for (var value in tabs) {
-        var sNode = document.getElementById(tabs[value][0]);
-        var spanNode = sNode.firstChild;
-        if (value == type) {
-            sNode.className = activeTableTab;
-            spanNode.innerHTML = tabs[value][1];
-        }
-        else {
-            sNode.className = tableTab;
-            spanNode.innerHTML = "<a href=\"javascript:show("+ value + ");\">" + tabs[value][1] + "</a>";
-        }
-    }
+function sub(){
+	var p1 = document.getElementById("validationCustom01").value;   //name
+	var p2 = document.getElementById("validationCustom03").value;   //address
+	var p3 = document.getElementById("validationCustom04").value;   //quantity
+	var p4 = document.getElementById("validationCustom05").value;   //pin
+	if(p1=="" && p2=="" && p3=="" && p4=="" ){
+		document.getElementById("place1").innerHTML="ERROR!!";
+		document.getElementById("place2").innerHTML="FILL FORM ACCURATELY";
+		document.getElementById("place1").style.color="red";
+		document.getElementById("place2").style.color="red";
+	}
+	else if(p1!="" && p2!="" && p3!="" && p4!=""){
+		document.getElementById("place1").innerHTML="SUCCESS!!";
+		document.getElementById("place2").innerHTML="ORDER PLACED";
+		document.getElementById("place1").style.color="green";
+		document.getElementById("place2").style.color="green";
+	}
 }
